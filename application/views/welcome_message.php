95624d251061468fef18b1899fbc9335b434e00f
Adrian Reyesta <muhammadadrianreyesta9d@gmail.com>
2022-08-03 10:24:52 +0700

    table tugas

---
+++ application/views/welcome_message.php
@@ -0,0 +1,74 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Welcome to CodeIgniter</title>
+
+	<style type="text/css">
+
+	#body {
+		margin: 0 15px 0 15px;
+		min-height: 96px;
+	}
+
+	#container {
+		margin: 10px;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+
+	table{
+		margin-left: 90px;
+	}
+
+	table tr td{
+		width: 600px;
+	}
+
+	.start{
+		text-align: start;
+		height: 30px;
+	}
+
+	h2{
+		background-color: black;
+		padding: 20px;
+		border-radius: 20px;
+		color: white;
+	}
+
+	h2:hover{
+		background-color: red;
+		transition: 0.3s;
+	}
+	</style>
+</head>
+<body>
+
+<div id="container">
+	<h2>INTRO CODEIGNITER 3</h2>
+	<table border>
+		<tr>
+			<th height="50px" style="background-color: green; color:aliceblue;">FAKE MY DEATH</th>
+		</tr>
+		<tr>
+			<th class="start">FAKE MY 22</th>
+		</tr>
+		<tr>
+			<td height="450px">
+
+			</td>
+		</tr>
+		<tr>
+			<th class="start">INTRO CodeIgniter 3</th>
+		</tr>
+	</table>
+
+</div>
+
+</body>
+</html>
